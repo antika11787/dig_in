a443fc9878d5dd2c239d25209d5b0d6bdab9b6c6
antika <antika.noor@bjitgroup.com>
2024-02-28 12:39:32 +0600

    backend deploy

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("/opt/render/project/src/uploads"));
 app.use(handleSyntaxError);
 app.use("/api/v1/", indexRouter);
 
